0d3aeaa7db21c6f2fd1f314fd79f5ecd983dc6b3
CocoDiet <chiblossom00@gmail.com>
2022-03-18 15:05:35 +0000

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * times_table - Prints the 9 times table
- * 
+ *
  *Return: no return
  */
 void times_table(void)
@@ -13,17 +13,20 @@ void times_table(void)
 	{
 		_putchar(48);
 	        for (b = 1; b <= 9; b++)	{
-				op = a * b;															      _putchar(44);
-																			      _putchar(32);
-																			      if (op <= 9)
-																			      {																                     _putchar(32);
-																				       _putchar(op + 48);
-																				}
-																				else
-																				{
-																					_putchar((op / 10) + 48);						   							      _putchar((op % 10) + 48);
-																																				      }
-		     																	}
-				    _putchar('\n');
+				op = a * b;															_putchar(44);
+					_putchar(32);
+				if (op <= 9)
+				{	
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else
+				{
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+		     		
+		}
+		_putchar('\n');
 	}
 }
