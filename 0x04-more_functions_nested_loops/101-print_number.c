20fd71df924abf0d92de0eeaa48d7cb786062ca8
CocoDiet <chiblossom00@gmail.com>
2022-03-20 16:39:43 +0000

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_number - prints an integer.
- *   * @n: input integer.
- *    * Return: no return.
- *     */
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 void print_number(int n)
 {
 	unsigned int m, d, count;
@@ -26,7 +26,7 @@ void print_number(int n)
 
 	d = m;
 	count = 1;
-	
+
 	while (d > 9)
 	{
 
