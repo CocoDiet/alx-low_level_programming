514e8b015e860fe40c3efbbdcb631e8df606e568
CocoDiet <chiblossom00@gmail.com>
2022-03-21 11:53:25 +0000

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,6 +30,7 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
+
 						if (h == 57 && g == 56 && e == 57 && i == 57)
 							break;
 						putchar(',');
@@ -45,4 +46,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
- }
+}
